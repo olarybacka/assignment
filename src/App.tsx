a38c044f31f5a56358fd584572d68bf8267f4549
Ola Rybacka <ola.rybacka@platform24.se>
2021-10-30 23:40:55 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
-import SplashScreen from './containers/SplashScreen';
-
+import SplashScreen from 'containers/SplashScreen'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import Home from 'containers/Home'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SplashScreen />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <SplashScreen />
+          </Route>
+          <Redirect to="/home" />
+        </Switch>
       </header>
     </div>
   )
--- src/containers/SplashScreen/SplashScreen.tsx
+++ src/containers/SplashScreen/SplashScreen.tsx
@@ -1,9 +1,12 @@
 import logo from 'assets/logo.svg'
 import { authenticate } from 'utils/authenticate'
+import { useHistory } from 'react-router-dom';
 
 const SplashScreen = () => {
+  const history = useHistory();
   authenticate()
     .then((res) => localStorage.setItem('token', res.AuthorizationToken?.Token))
+    .then(() => history.push('/home'))
     .catch((error) => console.log(error))
 
   return (
